8e849fa864947e4d875e06d7c18f73a5773b6391
ganesh <ganeshghorpade434@gmail.com>
2023-12-12 00:24:41 +0530

    Util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) =>{         //this is promises
+    (req,res,next) => { 
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+/* const asyncHandler = (fn) => async(req,res,next) => {  // this is the wraper
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success : false,
+            message : err.message
+        })
+    }
+} */
